7572201ec3c9645fd14f93b495c868b4f8a8ee12
Ivan Kovalkovskyi <vaniakov95@gmail.com>
2020-05-03 21:09:53 +0300

    Instance and elastic IP.

--- prod.tf
+++ prod.tf
@@ -34,10 +34,21 @@ resource "aws_security_group" "prod_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   
-  tags {
+  tags = {
     "Terraform" = true
-
   }
+}
 
+resource "aws_instance" "prod_web" {
+  ami           = "ami-0cebd9367bd2ef59f"
+  instance_type = "t2.nano"
 
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+
+  ]
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
 }
