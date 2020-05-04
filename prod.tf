346fe986696781e50a7d9a85d9c649a94783af90
Ivan Kovalkovskyi <vaniakov95@gmail.com>
2020-05-04 20:48:29 +0300

    Extract variables.

--- prod.tf
+++ prod.tf
@@ -1,10 +1,42 @@
+variable "region" {
+  default = "us-east-2"
+  type = string
+}
+variable "bucket_name" {
+  default = "tf-course-2020-05-04-ikova"
+  type = string
+}
+variable "web_image_id" {
+  default = "ami-0cebd9367bd2ef59f"
+  type = string
+}
+variable "web_instance_type" {
+  default = "t2.nano"
+  type = string
+}
+
+variable "whitelist" {
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "web_max_size" {
+  type = number
+  default = 1
+}
+
+variable "web_min_size" {
+  type = number
+  default = 1
+}
+
 provider "aws" {
   profile = "terraform"
-  region =  "us-east-2"
+  region = var.region
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket        = "tf-course-2020-05-04-ikova"
+  bucket        = var.bucket_name
   acl           = "private"
   force_destroy = true
 }
@@ -53,21 +85,21 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
   ingress {
     from_port   = 433
     to_port     = 433
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = var.whitelist
   }
-  
+
   tags = {
     "Terraform" = true
   }
@@ -75,16 +107,16 @@ resource "aws_security_group" "prod_web" {
 
 resource "aws_launch_template" "prod_web" {
   name_prefix   = "prod-web"
-  image_id      = "ami-0cebd9367bd2ef59f"
-  instance_type = "t2.nano"
+  image_id      = var.web_image_id
+  instance_type = var.web_instance_type
 }
 
 resource "aws_autoscaling_group" "prod_web" {
   availability_zones  = ["us-east-2a", "us-east-2b"]
   vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-  desired_capacity    = 2
-  max_size            = 2
-  min_size            = 2
+  desired_capacity    = 1
+  max_size            = var.web_max_size
+  min_size            = var.web_min_size
 
   launch_template {
     id      = aws_launch_template.prod_web.id
---
+++ terraform.tfvars
@@ -0,0 +1,8 @@
+region            =  "us-east-2"
+bucket_name       = "tf-course-2020-05-04-ikova"
+whitelist         = ["0.0.0.0/0"]
+web_name_prefix   = "prod-web"
+web_image_id      = "ami-0cebd9367bd2ef59f"
+web_instance_type = "t2.nano"
+web_max_size      = 2
+web_min_size      = 2
