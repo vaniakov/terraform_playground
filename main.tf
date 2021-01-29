977e3a468356e30fe49b5310a326852e25a416a6
Ivan Kovalkovskyi <vaniakov95@gmail.com>
2021-01-29 13:01:24 +0200

    change instance type

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   tags = {
     owner = "vaniakov95@gmail.com"
