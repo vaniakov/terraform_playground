1ac9652d4629f9efdf5f58dce956cabefe845988
Ivan Kovalkovskyi <vaniakov95@gmail.com>
2021-03-10 13:19:28 +0200

    Configurable instance ssh.

--- main.tf
+++ main.tf
@@ -2,45 +2,53 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#resource "aws_s3_bucket" "bucket" {
-#  bucket        = "omnibus-sources-ikova"
-#  force_destroy = true
-#  acl           = "private"
-#}
+# centos7 = ami-06cf02a98a61f9f5e
+# centos8 = ami-01ca03df4a6012157
 
-#resource "aws_instance" "centos8" {
-#  ami                         = "ami-01ca03df4a6012157"
-#  instance_type               = "t1.micro"
-#  associate_public_ip_address = true
-#  key_name                    = "falcon9"
-#
-#  tags = {
-#    owner = "vaniakov95@gmail.com"
-#  }
-#}
+variable "aws_ami" {
+  type    = string
+  default = "ami-06cf02a98a61f9f5e"
+  description = "The id of the machine image (AMI) to use for the server."
 
-resource "aws_instance" "centos7" {
-  ami                         = "ami-06cf02a98a61f9f5e"
+  validation {
+    # regex(...) fails if it cannot find a match
+    condition     = can(regex("^ami-", var.aws_ami))
+    error_message = "The aws_ami value must be a valid AMI id, starting with \"ami-\"."
+  }
+}
+
+variable "public_key" {
+  type    = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDp9bMARded99IjL703pATlN50vrzC/mXaLIYBk0Iy7FkAKTPpQU3/uFs3AriHFmdR2vanRouWQoonYC6kY/k/QPLkRzb3brBubA8Eqpt0oi9yF1bpi8uT8td4PhFg6Mbk4GLr1dztKffTIl0R0Po4/aA5BUoXyalpTlRra2EBeSzB0qWyUZTODPHwlsOU9IgQklknDTlMONxUcV0XGMGWpSrkJXXRGEIWQfeBBmgHsMwhSVKKTiDxKV/ryw3D3ryRrYVeyWksHHwpwJlxGh+TPdS4nCLu02NHOdfoZSSRpZmC2ecVsUZSDSPbkAJNMqmXmF9V45//wxPLTaYfQu1QpMUE1N0aPPK4yWls/qpIgxMAcoX8+xzUMruulHHgf6sCFhXzhvruMhDsitbBNHqQ5/WDMrYEmQmcJ6NfFEAnNpN6fLNPUqg706KgfDeZZSXvvgnMXU//JFMW/p+olBtrWs9TL2zEwkv7WLcuGiqsQKdKiWU507+gW9AFZ9w/HJ3ywV5+lRXKwebumLLwBsC5sR1f1oPLGiT8EKjbpdErJwuwoHKgZmEW9HW3j3dw+WrYD69aLxjQm95Oxx0/ZoGWr5s2NpiVlqii6W2WI6O1StZL5+qXV2PceJs3TBe4904LTC007Ibu4LWCSC8mPaZtWsXUH0NlWi8b7Sh8cY0pFuQ== ivan@MacBook-Pro.local"
+}
+
+variable "username" {
+  type    = string
+  default = "centos"
+}
+
+variable "key_name" {
+  type    = string
+  default = "scalr-iacp-key"
+}
+
+resource "aws_instance" "instance" {
+  ami                         = var.aws_ami
   instance_type               = "t1.micro"
   associate_public_ip_address = true
-  key_name                    = "falcon9"
+  key_name                    = var.key_name
 
   tags = {
     owner = "vaniakov95@gmail.com"
   }
 }
 
-output "centos7" {
-  description = "The public ip for ssh access"
-  value       = "ssh centos@${aws_instance.centos7.public_ip}"
+resource "aws_key_pair" "ssh-key" {
+  key_name   = var.key_name
+  public_key = var.public_key
 }
 
-#output "centos8" {
-#  description = "The public ip for ssh access"
-#  value       = "ssh centos@${aws_instance.centos8.public_ip}"
-#}
-
-resource "aws_key_pair" "ssh-key" {
-  key_name   = "falcon9"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDd1fgYf6aOQ4Aee6fv8vtcqZIxyjlmZdSZ7LG4BHpGgmzflQTDXDtjj99VlHIl+cR6c529EHp9tu+oLp1CEz+JU/tHXPw5bRs9SHVIZ2u5Dgv5IO5jGxnezAO4bLaqlVxT8vUAeECNEEXDNhoEitCzKSXx61g9wDtUMEsp72E8VSsxG9OpvMFrcGZs7wJtt495mp5coMaxm/ob08QzqCBoVGo2LOdx3sMcx6zPuZ7saZtFskJZA73DMkgvn/ppdD3FtFiHRH8kJvHyOKpSkvf5i6GDYOn5AhYTlVbhYpcKiufFKmI1ZEBFkfjFVkTymy9Ezy2rvfgOWjcTe1xro56EYhONGJxbOm0lIJYNBWonhTk5GwS3QaBJabEpAVwn9zWMoeQ+ZOMA6eiVivyqkaYoShT8E09rBc5kICbvCLlFw7JEBpDoqLyiaIHZ/VNsDksx3FeGbIfFsE5rgvpHc+2e7c6im36509MhJum0m3jrW6/hK2Ur5QPGQNJiFFT0DvEdl4M7JAnYkeIpgtlQYzQfi9EIW/aClaPrpNJy5AeQKxjTsXyDxK44KVGin1Ga3p7qN4H5B1eAhEUYvBlYtQTPbKvZbFygq29Ehp0Ie+CZ3CGeJGqhu425Tx2IhBtK5mV325GPH75Fo3SQPJR6QWp72FPgBKaPbBg55BW8PMkdIw== ikova@falcon9"
+output "public_ip" {
+  description = "The public ip for ssh access"
+  value       = "ssh ${var.username}@${aws_instance.instance.public_ip}"
 }
